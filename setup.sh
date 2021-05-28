14902292cdc8ce05f251ea9682901bd7f6cdcb0c
zuf1 <zlubis374@gmail.com>
2021-05-28 21:28:15 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,24 +1,4 @@
-﻿mkdir /var/lib/premium-script;
-echo "Masukkan nama host subdomain VPS, jika tidak tersedia, klik Enter"
-read -p "Hostname / Domain: " host
-echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/zuf1/ZET/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/zuf1/ZET/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
-wget https://raw.githubusercontent.com/zuf1/ZET/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
-wget https://raw.githubusercontent.com/zuf1/ZET/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/zuf1/ZET/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-wget https://raw.githubusercontent.com/zuf1/ZET/main/ins-vt.sh && chmod +x ins-vt.sh && sed -i -e 's/\r$//' ins-vt.sh && screen -S v2ray ./ins-vt.sh
-wget https://raw.githubusercontent.com/zuf1/ZET/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-rm -f /root/ssh-vpn.sh
-rm -f /root/sstp.sh
-rm -f /root/wg.sh
-rm -f /root/sodosok.sh
-rm -f /root/ssr.sh
-rm -f /root/ins-vt.sh
-rm -f /root/go.sh
-rm -f /root/ipsec.sh
-history -c
-echo "1.1" > /home/ver
+﻿echo "1.1" > /home/ver
 clear
 echo " "
 echo "Instalasi selesai !!"
