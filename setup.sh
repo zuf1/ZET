26230a75df1dd563ca0a70fca6b3667660cf05a4
zuf1 <zlubis374@gmail.com>
2021-05-29 09:59:49 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,7 +1,49 @@
-﻿echo "1.1" > /home/ver
+﻿if [ "${EUID}" -ne 0 ]; then
+echo "Anda perlu menjalankan skrip ini sebagai root"
+exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+echo "OpenVZ tidak didukung"
+exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/zuf1/ZET/main/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Izin Diterima...${NC}"
+else
+echo -e "${red}Izin ditolak!${NC}";
+echo "Hanya untuk Pengguna Premium"
+rm -f setup.sh
+exit 0
+fi
+mkdir /var/lib/premium-script;
+echo "Ingrese el nombre de host del subdominio VPS, si no está disponible, haga clic en Enter"
+read -p "Hostname / Domain: " host
+echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+wget https://raw.githubusercontent.com/zuf1/ZET/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/zuf1/ZET/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/zuf1/ZET/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/zuf1/ZET/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/zuf1/ZET/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/zuf1/ZET/main/ins-vt.sh && chmod +x ins-vt.sh && sed -i -e 's/\r$//' ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/zuf1/ZET/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+rm -f /root/ssh-vpn.sh
+rm -f /root/sstp.sh
+rm -f /root/wg.sh
+rm -f /root/sodosok.sh
+rm -f /root/ssr.sh
+rm -f /root/ins-vt.sh
+rm -f /root/go.sh
+rm -f /root/ipsec.sh
+history -c
+echo "1.1" > /home/ver
 clear
 echo " "
-echo "Instalasi selesai !!"
+echo "Se completó la Instalación!!"
 echo " "
 echo "=================================-Autoscript Premium-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
