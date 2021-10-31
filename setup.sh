5bc986d5926704de416fbc4e80a3127f3577fd36
zuf1 <zlubis374@gmail.com>
2021-10-31 14:59:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,4 @@
-if [ "${EUID}" -ne 0 ]; then
-echo "You need to run this script as root"
-exit 1
-fi
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/zuf1/ZET/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${green}Permission Accepted...${NC}"
-fi
+#!/bin/bash
 mkdir /var/lib/premium-script;
 echo "Enter the VPS Subdomain Hostname, if not available, please click Enter"
 read -p "Hostname / Domain: " host
