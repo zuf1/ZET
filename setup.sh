c5a69884e6b11577747b64c5020098f34a7670e4
zuf1 <zlubis374@gmail.com>
2021-10-31 11:24:15 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,12 @@ if [ "${EUID}" -ne 0 ]; then
 echo "You need to run this script as root"
 exit 1
 fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-echo "OpenVZ is not supported"
-exit 1
-fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/jhall031/testkdn/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/zuf1/ZET/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
